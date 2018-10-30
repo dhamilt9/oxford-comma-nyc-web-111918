798cc558ebe1800ebb021fc3c201653e4d414f6c
Daniel Hamilton <danhamilton16@gmail.com>
2018-10-30 20:14:59 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,19 @@
 def oxford_comma(array)
-
+  if array.size==1
+    return array[0]
+  elsif array.size==2
+    return "#{array[0]} and #{array[1]}"
+  else
+    returnstring=""
+    array.each_with_index do |item, index|
+      if index!=0
+        returnstring=returnstring<<", "
+      end
+      if index+1==array.size
+        returnstring=returnstring<<"and "
+      end
+      returnstring=returnstring<<item
+    end
+    return returnstring
+  end
 end
