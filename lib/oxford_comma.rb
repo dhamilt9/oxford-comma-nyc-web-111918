bb873812dffdbac3ac15cc166ca4e491473c875c
Daniel Hamilton <danhamilton16@gmail.com>
2018-10-30 20:14:51 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,6 +1,8 @@
 def oxford_comma(array)
   if array.size==1
     return array[0]
+  elsif array.size==2
+    return "#{array[0]} and #{array[1]}"
   else
     returnstring=""
     array.each_with_index do |item, index|
